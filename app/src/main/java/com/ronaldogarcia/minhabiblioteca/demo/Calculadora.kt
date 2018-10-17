203fe6cbda491cccb336c1b02a33a66d77e2532b
Ronaldo <garcia.coelho.ronaldo@gmail.com>
2018-10-16 23:03:27 -0300

    Toast customizado v1.2

--- app/src/main/java/com/ronaldogarcia/minhabiblioteca/demo/Calculadora.kt
+++ app/src/main/java/com/ronaldogarcia/minhabiblioteca/demo/Calculadora.kt
@@ -1,7 +1,8 @@
 package com.ronaldogarcia.minhabiblioteca.demo
 
+import com.ronaldogarcia.minhabiblioteca.toast.CustomToast
+
 class Calculadora {
 
     fun somar(v1: Int, v2: Int): Int = v1 + v2
-
 }
--- app/src/main/java/com/ronaldogarcia/minhabiblioteca/toast/CustomToast.kt
+++ app/src/main/java/com/ronaldogarcia/minhabiblioteca/toast/CustomToast.kt
@@ -3,14 +3,23 @@ package com.ronaldogarcia.minhabiblioteca.toast
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import com.ronaldogarcia.minhabiblioteca.R
 
 class CustomToast(val context: Context, val inflater: LayoutInflater) {
 
-    fun showToast(text: String) {
-        val customToastroot = inflater.inflate(R.layout.custom_toast, null)
+    companion object {
+        val WARNING = 1
+        val SUCCESS = 2
+        val INFO = 3
+        val ERROR = 4
+    }
+
+    fun showToast(text: String, type: Int) {
+
+        val customToastroot = inflater.inflate(getLayout(type), null)
         val customtoast = Toast(context)
 
         val textView = customToastroot.findViewById<TextView>(R.id.textView1)
@@ -21,6 +30,15 @@ class CustomToast(val context: Context, val inflater: LayoutInflater) {
         customtoast.duration = Toast.LENGTH_LONG
 
         customtoast.show()
+    }
+
+    private fun getLayout(type: Int): Int {
+        return when (type) {
+            WARNING -> R.layout.custom_toast_warning
+            SUCCESS -> R.layout.custom_toast_success
+            INFO -> R.layout.custom_toast_info
+            else -> R.layout.custom_toast_error
+        }
     }
 
 }
